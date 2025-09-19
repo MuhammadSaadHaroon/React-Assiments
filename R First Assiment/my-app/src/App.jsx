491698ade9b72ca690a38c9adb2f53186ecc107b
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-09-19 19:26:25 +0500

    Added my-app folder as normal project

---
+++ R First Assiment/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App 🚀</h1>
+      <p>This is my first React project.</p>
+      
+      <img 
+        src="https://i.ytimg.com/vi/fDrJZ5WI91g/hqdefault.jpg" 
+        alt="Random Example" 
+      />
+
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>Frontend Development</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
